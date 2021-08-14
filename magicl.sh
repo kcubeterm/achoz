7d93da0a7379b49cf081c619971e1a5494cb2dc8
c+aue <cregox@ahoxus.org>
2021-08-14 07:41:31 +0100

    add image magic

--- magicl.sh
+++ magicl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 
-# reduce music library
-# into 16k opus, while keeping album art
+# reduce:
+# music into 16k opus, while keeping album art
+# image into 1024 jpg, png, or svg
 
 # use this to better organize big folders:
 #mkdir .before; mkdir .error
+
+# and almost this to help preventing multiple jpeg compressions
+#mv *.jpg *.jpeg
 
 for i in *.m??; do
 	# main activity: compress mp3 down to 10%:
@@ -17,6 +21,13 @@ for i in *.m??; do
 	mv "$i" .error/
 done
 
+for i in *.jpg *.png; do
+	# main activity: compress jpeg down to 10%:
+	ffmpeg -hide_banner -y -i "$i" -vf scale=1024:-1 "${i%.*}.jpeg" && mv "$i" .before/
+
+	mv "$i" .error/
+done
+
 # notify when finished, d'uh:
-echo zzz finished $TITLE | termux-notification
+echo finished $TITLE | termux-notification
 termux-vibrate -f
