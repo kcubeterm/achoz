61a150b3a1cf7fde82eaf644dfa4330ac7bb00a5
kcubeterm <kcubeterm.gmail.com>
2021-11-26 16:23:39 +0530

    cli.js: check search engine health... and then start crawling then indexing

--- cli.js
+++ cli.js
@@ -1,34 +1,75 @@
 #!/usr/bin/env node
 
-const appRoot = require('app-root-path')
-const exec = require('child_process').exec
+const appRoot = require('app-root-path');
+const exec = require('child_process').exec;
+const fetch = require("node-fetch");
+const fs = require('fs')
+try {
+    var config = fs.readFileSync(`${appRoot}/config.json`, 'utf8');
+} catch (err) {
+    if (err.code == 'ENOENT') {
+        return console.log("config.json not found");
+    }
+}
+
+config = JSON.parse(config);
+TypesenseHost = config.TypesenseHost;
+Typesense_api = config.TypesenseApi;
 
 
-let crawlerAndindexer = new Promise((resolve, reject) => {
-    exec(`node ${appRoot}/crawler/crawler.js`, (err, stdout, stderr) => {
-        if (err) {
-            console.warn(err)
-        }
-        resolve();
-    }).stdout.on('data', function (data) {
-        console.log(data);
-    });
-    
-})
 
-crawlerAndindexer.then(() => {
-    console.log('Indexing documents')
-    exec(`bash ${appRoot}/crawler/indexer.sh`, (err, stdout, stderr) => {
-        if (err) {
-            console.warn(err)
-        }
-    }).stdout.on('data', function (data) {
-        console.log(data);
-    });
 
-}).then(() => {
-    exec(`node ${appRoot}/server.js`).stdout.on('data', function (data) {
-        console.log(data);
+
+
+// checking health of search engine 
+function health() {
+    var url = `${TypesenseHost}/health`
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            if (data.ok) {
+                console.log("Search engine is running ")
+                server()
+            } else {
+                process.exit(1)
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+function server() {
+
+    let crawlerAndindexer = new Promise((resolve, reject) => {
+        exec(`node ${appRoot}/crawler/crawler.js`, (err, stdout, stderr) => {
+            if (err) {
+                console.warn(err)
+            }
+            resolve();
+        }).stdout.on('data', function (data) {
+            console.log(data);
+        });
+        
     })
-})
+    
+    crawlerAndindexer.then(() => {
+        console.log('Indexing documents')
+        exec(`bash ${appRoot}/crawler/indexer.sh`, (err, stdout, stderr) => {
+            if (err) {
+                console.warn(err)
+            }
+        }).stdout.on('data', function (data) {
+            console.log(data);
+        });
+    
+    }).then(() => {
+        exec(`node ${appRoot}/server.js`).stdout.on('data', function (data) {
+            console.log(data);
+        })
+    })
 
+}    
+
+health()
