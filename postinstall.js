c8428036fed9a9b0633462e95a482abb996249c8
kcubeterm <kcubeterm@gmail.com>
2021-11-27 19:33:27 +0530

    postinstall.js: install typesense server

--- postinstall.js
+++ postinstall.js
@@ -4,17 +4,31 @@ const http = require('http')
 const fs = require('fs');
 const os = require('os')
 const config = require("./config.json")
+const targz = require('targz')
 
-var url = 'http://localhost:8989/typesense-server'
-const file = fs.createWriteStream('./bin/typesense-achoz');
+var url = 'https://dl.typesense.org/releases/0.21.0/typesense-server-0.21.0-linux-amd64.tar.gz'
+const file = fs.createWriteStream('/tmp/server.tar.gz');
 
 const request = http.get(url, function (response) {
   response.pipe(file);
-  fs.chmodSync("./bin/typesense-achoz", 0755)
+
   config.TypesenseApi = createApiKey(40)
   filename = "./config.json"
   config.LocalDataDir = os.homedir + '/.achoz'
   fs.writeFileSync(filename, JSON.stringify(config, null, 2));
+  const targz = require('targz')
+  targz.decompress({
+    src: '/tmp/server.tar.gz',
+    dest: '/tmp'
+  }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Done!");
+      fs.copyFileSync('/tmp/typesense-server', './bin/typesense-achoz')
+      fs.chmodSync("./bin/typesense-achoz", 0755) // data integrity wip
+    }
+  });
 
 });
 
